30d6f7f5e594029e01ba82224b03f791a35fc583
klinelv <klinelv@ucmail.uc.edu>
2026-09-17 20:01:08 -0400

    Added variables, trying to understand :o

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         int intOperandA = 1;
+        int intOperandB = 7;
+        int intSum = 24;
+        int intDifference = 12;
+        int intProduct = 13;
+        int intQuotient = 2;
+        int intModulo = 8;
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 2.50;
+        double doubleSum = 6.90;
+        double doubleProduct = 4.21;
+        double doubleDifference = 1.22;
+        double doubleQuotient = 5.55;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum which uses ints of + intOperandA = " + intOperandA + " and intOperandB = " + intOperandB + " is " + intSum);
+
+
+
     }
 }
