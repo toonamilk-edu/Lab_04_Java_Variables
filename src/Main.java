ef525a58a986e36bd724ab48a3e7beee9a44ae19
Logan Kline <klinelv@ucmail.uc.edu>
2026-09-20 10:16:53 -0400

    finished #10

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,16 @@ public class Main {
         System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
         intModulo = intOperandA % intOperandB;
         System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
-        
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+
 
 
     }
