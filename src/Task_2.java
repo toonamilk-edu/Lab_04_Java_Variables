358dfaed0a19e3ad5245ec7c56c6f6da6186f905
Logan Kline <klinelv@ucmail.uc.edu>
2026-09-20 11:50:41 -0400

    working on task 2

---
+++ src/Task_2.java
@@ -0,0 +1,11 @@
+public class Task_2 {
+    public static void main(String[] args) {
+    double winterCosts = 1000.00;
+    double springCosts = 500.00;
+    double summerCosts = 750.00;
+    double fallCosts = 600.00;
+    double totalMaintenanceCosts = winterCosts + springCosts + summerCosts + fallCosts;
+    System.out.println("The total maintenance costs for the year is " + "winter costs " + winterCosts + ", spring costs " + springCosts + ", summer costs " + summerCosts + ", and fall costs " + fallCosts + " = " + totalMaintenanceCosts);
+
+    }
+}
