8db378569ed01a46d029e6a7dfaa84adc2763c39
Logan Kline <klinelv@ucmail.uc.edu>
2026-09-20 22:25:38 -0400

    finished task 4

---
+++ src/Task_4.java
@@ -0,0 +1,11 @@
+public class Task_4 {
+    public static void main(String[] args) {
+        int numToExamine = 7;
+        int result = numToExamine % 2;
+        System.out.println("a value of 0 means the number is even (no remainder when dividing by 2), and a value of 1 means the number is odd (has remainder when dividing by 2)");
+        System.out.println("and the result of " + numToExamine + " % 2 is " + result);
+
+
+    }
+}
+
